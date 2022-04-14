40328305723ad5096ae3055e50c9318daa3ab8e5
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-14 18:15:09 +0900

    some_sums追加

---
+++ some_sums/SomeSums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] split = Console.ReadLine().Split(' ');
+        int n = int.Parse(split[0]);
+        int a = int.Parse(split[1]);
+        int b = int.Parse(split[2]);
+
+        int result = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            int sum = CalcSumOrder(i);
+            if (sum >= a && sum <= b)
+                result += i;
+        }
+
+        Console.WriteLine($"{result}");
+    }
+
+    static int CalcSumOrder(int value)
+    {
+        int sum = 0;
+        int tmp = value;
+        while (0 < tmp)
+        {
+            sum += (tmp % 10);
+            tmp = (tmp / 10);
+        }
+        return sum;
+    }
+}
