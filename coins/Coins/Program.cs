3f01069fa1d2dd7a29bdfed229bb91e5e7835f2b
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-14 18:14:44 +0900

    coins追加

---
+++ coins/Coins/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int x = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for(int i = 0; i <= a; i++)
+        {
+            for(int j = 0; j <= b; j++)
+            {
+                for (int k = 0; k <= c; k++)
+                {
+                    if (x == i * 500 + j * 100 + k * 50)
+                        sum++;
+                }
+            }
+        }
+
+        Console.WriteLine($"{sum}");
+    }
+}
