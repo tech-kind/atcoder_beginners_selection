c6cba919a41c4c1c89f366e158a0b9fee14fdcf0
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-14 14:57:10 +0900

    product追加

---
+++ product/Product/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] split = Console.ReadLine().Split(' ');
+        int a = int.Parse(split[0]);
+        int b = int.Parse(split[1]);
+
+        if (a % 2 == 0 || b % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+    }
+}
