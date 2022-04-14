9e5c7eb7f7b54cdcfa8e0bf3ec4a7fc1d864659c
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-15 08:17:00 +0900

    card_game_for_two追加

---
+++ card_game_for_two/CardGameForTwo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] split = Console.ReadLine().Split(' ');
+
+        int[] cards = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            cards[i] = int.Parse(split[i]);
+        }
+        
+        Array.Sort(cards);
+        Array.Reverse(cards);
+
+        int alicePoint = 0;
+        int BobPoint = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                alicePoint += cards[i];
+            }
+            else
+            {
+                BobPoint += cards[i];
+            }
+        }
+
+        Console.WriteLine($"{alicePoint - BobPoint}");
+    }
+}
