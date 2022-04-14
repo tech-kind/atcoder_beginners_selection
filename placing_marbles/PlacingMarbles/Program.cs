d7138e7f89dc463be95a5490bbdc977f8b4e9502
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-14 14:57:21 +0900

    placing_marbles追加

---
+++ placing_marbles/PlacingMarbles/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+        int count = CountChar(s, '1');
+        Console.WriteLine($"{count}");
+    }
+
+    static int CountChar(string s, char c)
+    {
+        return s.Length - s.Replace(c.ToString(), "").Length;
+    }
+}
