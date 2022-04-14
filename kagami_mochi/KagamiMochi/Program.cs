2a5ead649bb8b08e537c65c83eae6e974db6ee7c
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-15 08:17:10 +0900

    kagami_mochi追加

---
+++ kagami_mochi/KagamiMochi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+        Array.Sort(numbers);
+
+        int result = 0;
+        int tmp = -1;
+        for (int i = 0;i < n;i++)
+        {
+            if (numbers[i] > tmp)
+            {
+                result++;
+                tmp = numbers[i];
+            }
+        }
+        Console.WriteLine($"{result}");
+    }
+}
