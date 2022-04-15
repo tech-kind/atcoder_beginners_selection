d95f636ea4f9cc90b5eb8956a0e58a1bddb4f589
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-15 13:55:55 +0900

    hakuchumu追加

---
+++ hakuchumu/Hakuchumu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        input = input.Replace("eraser", "");
+        input = input.Replace("erase", "");
+        input = input.Replace("dreamer", "");
+        input = input.Replace("dream", "");
+
+        Console.WriteLine(input.Length == 0 ? "YES" : "NO");
+    }
+}
